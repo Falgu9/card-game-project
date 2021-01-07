24d1f180d6085f4d68978b21dfa63542f28f6705
OKUTUCU hamza <alkarose@hotmail.fr>
2021-01-07 12:16:56 +0100

    Update Card.h

--- Card.h
+++ Card.h
@@ -8,26 +8,30 @@ class Card
 	friend std::ostream& operator<<(std::ostream& os, Card const& card);
 
 private:
-	static std::string ranks[13];
+	static std::string ranks[14];
 	static std::string suits[4];
 	
 	int rank;
 	int suit;
+	std::string symbol;
 	int value;
 
 public:
 	Card();
 	Card(int rank, int suit);
+	Card(int rank, int suit, std::string symbol);
 	Card(int rank, int suit, int value);
 
 	int getRank() const;
 	int getSuit() const;
+	std::string getSymbol() const;
 	int getValue() const;
 	
 	void setRank(int rank);
 	void setSuit(int suit);
+	void setSymbol(std::string symbol);
 	void setValue(int value);
-
+	
 	bool operator<=(const Card& a) const;
 	bool operator>=(const Card& a) const;
 	bool operator<(const Card& a) const;
