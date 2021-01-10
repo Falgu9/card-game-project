db320ce42026af4b7c65b33c8c74578112b4a872
OKUTUCU hamza <alkarose@hotmail.fr>
2021-01-10 19:32:48 +0100

    Upload New File

---
+++ v2/Belote.h
@@ -0,0 +1,38 @@
+#ifndef __BELOTE__H__
+#define __BELOTE__H__
+
+#include "CardGame.h"
+#include "Team.h"
+
+class Belote : public CardGame
+{
+	friend std::ostream& operator<<(std::ostream& os, Belote const& belote);
+private:
+	Team **teams;
+	Linkedlist<Card*> *playedCards;
+	int indexTeamContract;
+	int atout;
+public:
+	Belote();
+	~Belote();
+
+	void makeTeam();
+	void setValueOfCards();
+	void setValueOfCards(int rank, int suit, int value);
+	void retourne();
+	bool retourneHasBeenTaken(Card *retourne);
+	void play();
+	void plis();
+	void setPointsOfTeam(int indexPlis);
+	void setNewOrderOfPlayers();
+	bool canPlayNonAtout();
+	void playCard();
+	void playAnyCard();
+	bool playNonAtout();
+	void playAtout();
+	bool haveNoAtout();
+	bool haveStrongerAtout();
+	bool partenerIsMaster();
+};
+
+#endif
