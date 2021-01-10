eac6dab00bbedf103bfdfa71824f94a938c6f02c
OKUTUCU hamza <alkarose@hotmail.fr>
2021-01-10 19:35:05 +0100

    Upload New File

---
+++ v2/Card.h
@@ -0,0 +1,46 @@
+#ifndef __CARD__H__
+#define __CARD__H__
+
+#include <string>
+
+class Card
+{
+	friend std::ostream &operator<<(std::ostream &os, Card const &card);
+
+private:
+	static std::string ranks[14];
+	static std::string suits[4];
+
+	int rank;
+	int suit;
+	std::string symbol;
+	int value;
+	std::string couleur;
+
+public:
+	Card();
+	Card(int rank, int suit);
+	Card(int rank, int suit, std::string symbol);
+	Card(int rank, int suit, int value, std::string symbol);
+	Card(int rank, int suit, int value);
+
+	int getRank() const;
+	int getSuit() const;
+	std::string getSymbol() const;
+	int getValue() const;
+	std::string getCouleur() const;
+
+	void setRank(int rank);
+	void setSuit(int suit);
+	void setSymbol(std::string symbol);
+	void setValue(int value);
+
+	bool operator<=(const Card &a) const;
+	bool operator>=(const Card &a) const;
+	bool operator<(const Card &a) const;
+	bool operator>(const Card &a) const;
+	bool operator==(const Card &a) const;
+	bool operator!=(const Card &a) const;
+};
+
+#endif
